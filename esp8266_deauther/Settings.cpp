3d36cb3f3c8f9a05166bc985aaa02b78cbaf430c
N0vaPixel <aurelien.hernandez27@gmail.com>
2017-03-03 23:55:52 +0100

    Added setting options to scan hidden APs and to hide your own

--- esp8266_deauther/Settings.cpp
+++ esp8266_deauther/Settings.cpp
@@ -15,6 +15,16 @@ void Settings::load(){
     for(int i=0;i<ssidLen;i++) ssid += (char)EEPROM.read(ssidAdr+i);
     for(int i=0;i<passwordLen;i++) password += (char)EEPROM.read(passwordAdr+i);
     
+    ssidHidden = (bool)EEPROM.read(ssidHiddenAdr);
+
+    if((int)EEPROM.read(apChannelAdr) >= 1 && (int)EEPROM.read(apChannelAdr) <= 11){
+      apChannel = (int)EEPROM.read(apChannelAdr);
+    } else {
+      reset();
+    }
+    
+    apScanHidden = (bool)EEPROM.read(apScanHiddenAdr);
+    
     deauthReason = EEPROM.read(deauthReasonAdr);
     attackTimeout = eepromReadInt(attackTimeoutAdr);
     attackPacketRate = EEPROM.read(attackPacketRateAdr);
@@ -28,9 +38,13 @@ void Settings::reset(){
   
   ssid = "pwned";
   password = "deauther"; //must have at least 8 characters
+  ssidHidden = false;
+  apChannel = 1;
 
   ssidLen = ssid.length();
   passwordLen = password.length();
+  
+  apScanHidden = true;
     
   deauthReason = 0x01;
   attackTimeout = 5*60;
@@ -47,11 +61,15 @@ void Settings::save(){
   ssidLen = ssid.length();
   passwordLen = password.length();
   
-  EEPROM.write(ssidLenAdr,ssidLen);
-  EEPROM.write(passwordLenAdr,passwordLen);
-  
+  EEPROM.write(ssidLenAdr, ssidLen);
+  EEPROM.write(passwordLenAdr, passwordLen);
   for(int i=0;i<ssidLen;i++) EEPROM.write(ssidAdr+i,ssid[i]);
   for(int i=0;i<passwordLen;i++) EEPROM.write(passwordAdr+i,password[i]);
+
+  EEPROM.write(ssidHiddenAdr, ssidHidden);
+  EEPROM.write(apChannelAdr, apChannel);
+
+  EEPROM.write(apScanHiddenAdr, apScanHidden);
   
   EEPROM.write(deauthReasonAdr, deauthReason);
 
@@ -72,8 +90,11 @@ void Settings::info(){
   Serial.println("settings:");
   Serial.println("SSID: "+ssid);
   Serial.println("SSID length: "+(String)ssidLen);
+  Serial.println("SSID hidden: "+(String)ssidHidden);
   Serial.println("password: "+password);
   Serial.println("password length: "+(String)passwordLen);
+  Serial.println("channel: "+(String)apChannel);
+  Serial.println("Scan hidden APs: "+(String)apScanHidden);
   Serial.println("deauth reson: "+(String)(int)deauthReason);
   Serial.println("attack timeout: "+(String)attackTimeout);
   Serial.println("attack packet rate: "+(String)attackPacketRate);
@@ -86,7 +107,10 @@ String Settings::get(){
   String json = "{";
   
   json += "\"ssid\":\""+ssid+"\",";
+  json += "\"ssidHidden\":"+(String)ssidHidden+",";
   json += "\"password\":\""+password+"\",";
+  json += "\"apChannel\":"+(String)apChannel+",";
+  json += "\"apScanHidden\":"+(String)apScanHidden+",";
   json += "\"deauthReason\":"+(String)(int)deauthReason+",";
   json += "\"attackTimeout\":"+(String)attackTimeout+",";
   json += "\"attackPacketRate\":"+(String)attackPacketRate+",";
