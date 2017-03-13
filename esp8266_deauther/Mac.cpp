1d13f78215d6696f414439ee45e532f3dfc36c67
N0vaPixel <aurelien.hernandez27@gmail.com>
2017-03-13 20:58:53 +0100

    Minor improvements

--- esp8266_deauther/Mac.cpp
+++ esp8266_deauther/Mac.cpp
@@ -1,12 +1,9 @@
 #include "Mac.h"
 
 Mac::Mac(){
-  adress[0] = 0x00;
-  adress[1] = 0x00;
-  adress[2] = 0x00;
-  adress[3] = 0x00;
-  adress[4] = 0x00;
-  adress[5] = 0x00;
+  for(int i=0;i<6;i++){
+    adress[i] = 0x00;
+  }
 }
 
 void Mac::set(uint8_t first, uint8_t second, uint8_t third, uint8_t fourth, uint8_t fifth, uint8_t sixth){
@@ -23,19 +20,16 @@ void Mac::setAt(uint8_t first, int num){
 }
 
 void Mac::setMac(Mac adr){
-  adress[0] = adr._get(0);
-  adress[1] = adr._get(1);
-  adress[2] = adr._get(2);
-  adress[3] = adr._get(3);
-  adress[4] = adr._get(4);
-  adress[5] = adr._get(5);
+  for(int i=0;i<6;i++){
+    adress[i] = adr._get(i);
+  }
 }
 
 bool Mac::valid(){
   for(int i=0;i<6;i++){
-    if(adress[i] != 0xFF && adress[i] != 0x00) return true;
+    if(adress[i] == 0xFF || adress[i] == 0x00) return false;
   }
-  return false;
+  return true;
 }
 
 String Mac::toString(){
@@ -50,17 +44,11 @@ String Mac::toString(){
   return value;
 }
 
-void Mac::_print(){
-  Serial.print(Mac::toString());
-}
+void Mac::_print(){Serial.print(Mac::toString());}
 
-void Mac::_println(){
-  Serial.println(Mac::toString());
-}
+void Mac::_println(){Serial.println(Mac::toString());}
 
-uint8_t Mac::_get(int num){
-  return adress[num];
-}
+uint8_t Mac::_get(int num){return adress[num];}
 
 bool Mac::compare(Mac target){
   for(int i=0;i<6;i++){
