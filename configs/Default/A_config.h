f03d9cabd7a39044639fca6e4ad9f2103cbcd414
Stefan Kremser <stef.1497@gmail.com>
2018-04-23 08:30:46 +0200

    Note about Neopixel parameters

--- configs/Default/A_config.h
+++ configs/Default/A_config.h
@@ -41,7 +41,16 @@
 // === Settings for Neopixel LED === //
 #define LED_NEOPIXEL_NUM 1
 #define LED_NEOPIXEL_PIN 9
-// if it doesn't work try changing NEO_GRB to NEO_GRBW 
+
+
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = Arduino pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
 #define LED_NEOPIXEL Adafruit_NeoPixel(LED_NEOPIXEL_NUM, LED_NEOPIXEL_PIN, NEO_GRB + NEO_KHZ800)
 
 // ===================== DISPLAY CONFIG ==================== //
