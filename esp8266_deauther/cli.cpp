9c6eb4c2ba3d09f1f3fc207177fe80092760ef03
Spacehuhn <github@spacehuhn.com>
2020-01-12 14:28:14 +0100

    CLI clear command

--- esp8266_deauther/cli.cpp
+++ esp8266_deauther/cli.cpp
@@ -38,6 +38,14 @@ namespace cli {
         Command cmd_help = cli.addCommand("help", [](cmd* c) {
             debugln(cli.toString());
         });
+        cmd_help.setDescription("  Prints the list of commands that you see right now");
+
+        Command cmd_clear = cli.addCommand("clear", [](cmd* c) {
+            for (uint8_t i = 0; i<100; ++i) {
+                debugln();
+            }
+        });
+        cmd_clear.setDescription("  Clears serial output (by spamming line breaks :P)");
 
         Command cmd_ram = cli.addCommand("ram", [](cmd* c) {
             debug("Size: ");
@@ -56,6 +64,7 @@ namespace cli {
             debug(system_get_free_heap_size() / (81920 / 100));
             debugln("%)");
         });
+        cmd_ram.setDescription("  Prints memory usage");
 
         Command cmd_scan = cli.addCommand("scan", [](cmd* c) {
             Command cmd(c);
@@ -69,12 +78,15 @@ namespace cli {
             scan::searchSTs(scan_time);
         });
         cmd_scan.addArg("t/ime", "14");
-        cmd_scan.setDescription("-t or -time: station scan time in seconds (min=1,default=14)");
+        cmd_scan.setDescription(
+            "  Scan for WiFi devices\n"
+            "  -t or -time: station (client) scan time in seconds (min=1,default=14)"
+            );
 
         Command cmd_results = cli.addCommand("results", [](cmd* c) {
             scan::printResults();
         });
-        cmd_results.setDescription("Prints list of scan results [access points (networks) and stations (clients)]");
+        cmd_results.setDescription("  Prints list of scan results [access points (networks) and stations (clients)]");
     }
 
     void parse(const char* input) {
