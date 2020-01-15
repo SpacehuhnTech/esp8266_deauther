9ff69cea80fc196484fa537569a08c338cd307ab
Spacehuhn <github@spacehuhn.com>
2020-01-15 21:04:33 +0100

    Packet injection module

---
+++ esp8266_deauther/packetinjector.h
@@ -0,0 +1,17 @@
+/*
+   Copyright (c) 2020 Stefan Kremser
+   This software is licensed under the MIT License. See the license file for details.
+   Source: github.com/spacehuhn/esp8266_deauther
+ */
+
+#pragma once
+
+#include <stdint.h> // uint8_t, uint16_t
+
+namespace packetinjector {
+    bool send(uint8_t ch, uint8_t* buf, uint16_t len);
+    bool deauth(uint8_t ch, uint8_t* from, uint8_t* to);
+    bool disassoc(uint8_t ch, uint8_t* from, uint8_t* to);
+    bool beacon(uint8_t ch, uint8_t* from, const char* ssid, bool wpa2);
+    bool probe(uint8_t ch, uint8_t* from, const char* ssid);
+}
