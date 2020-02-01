ba54fbff974e43c6fdab59f36097fa233a4e5ac3
Spacehuhn <github@spacehuhn.com>
2020-02-01 18:54:32 +0100

    Assisted beacon attack

--- esp8266_deauther/cli.cpp
+++ esp8266_deauther/cli.cpp
@@ -21,6 +21,13 @@ extern "C" {
   #include "user_interface.h"
 }
 
+#define CLI_READ_RES()\
+    res = read_and_wait();\
+    if (res == "exit") {\
+        debugln("Ok byeee");\
+        return;\
+    }
+
 namespace cli {
     // ===== PRIVATE ===== //
     SimpleCLI cli; // !< Instance of SimpleCLI library
@@ -56,76 +63,106 @@ namespace cli {
             debugln("Good morning friend.");
 
             // Command
-            while (!(res == "scan")) {
+            do {
                 debugln("What can I do for you today?\n"
                         "Remember that you can always escape by typing 'exit'\n"
-                        "  scan: Search for WiFi networks and clients");
-                res = read_and_wait();
-                if (res == "exit") {
-                    debugln("Ok byeee");
-                    return;
-                }
-            };
+                        "  scan: Search for WiFi networks and clients\n"
+                        "  beacon: Send WiFi network advertisement beacons\n"
+                        "  deauth: Disrupt WiFi connections"
+                        );
+                CLI_READ_RES();
+            } while (!(res == "scan" || res == "beacon" || res == "deauth"));
 
             if (res == "scan") {
                 // Scan mode
-                while (!(res == "ap" || res == "st" || res == "ap+st")) {
+                do {
                     debugln("Scan mode\n"
                             "  ap: Access Points (WiFi networks)\n"
                             "  st: Stations (WiFi clients)\n"
                             "  ap+st: Access Points and Stations");
-                    res = read_and_wait();
-                    if (res == "exit") {
-                        debugln("Ok byeee");
-                        return;
-                    }
-                };
+                    CLI_READ_RES();
+                } while (!(res == "ap" || res == "st" || res == "ap+st"));
 
                 cmd += "scan -m " + res;
 
                 // Scan time and channel(s)
                 if (res != "ap") {
 // Scan time
-                    while (!(res.toInt() > 0)) {
+                    do {
                         debugln("Scan time\n"
                                 "  >1: Station scan time in seconds");
-                        res = read_and_wait();
-                        if (res == "exit") {
-                            debugln("Ok byeee");
-                            return;
-                        }
-                    };
+                        CLI_READ_RES();
+                    } while (!(res.toInt() > 0));
                     cmd += " -t " + res;
 
 // Scan on channel(s)
                     debugln("Scan on channel(s)\n"
                             "  1-14: WiFi channel(s) to search on (for example: 1,6,11)");
-
-                    res = read_and_wait();
-                    if (res == "exit") {
-                        debugln("Ok byeee");
-                        return;
-                    }
-
+                    CLI_READ_RES();
                     cmd += " -ch " + res;
                 }
 
                 // Retain scan results
-                while (!(res == String('y') || res == String('n'))) {
+                do {
                     debugln("Retain previous scan results\n"
                             "  y: Yes\n"
                             "  n: No");
-                    res = read_and_wait();
-                    if (res == "exit") {
-                        debugln("Ok byeee");
-                        return;
-                    }
-                };
+                    CLI_READ_RES();
+                } while (!(res == String('y') || res == String('n')));
 
                 if (res == String('y')) {
                     cmd += " -r";
                 }
-            }
+            } else if (res == "beacon") {
+                // SSIDs
+                debugln("SSIDs (network names) for example \"network A\",\"network B\"\n");
+                CLI_READ_RES();
+                cmd += "beacon -s " + res;
+
+                // From
+                do {
+                    debugln("Transmitter and sender MAC address\n"
+                            "  for example '00:20:91:aa:bb:5cc\n"
+                            "  random: generate random MAC address");
+                    CLI_READ_RES();
+                } while (!(res.length() == 17 || res == "random"));
+                cmd += " -from " + res;
+
+                // To
+                do {
+                    debugln("Receiver MAC address\n"
+                            "  for example '00:20:91:aa:bb:5cc\n"
+                            "  broadcast: visible to all devices");
+                    CLI_READ_RES();
+                } while (!(res.length() == 17 || res == "broadcast"));
+                cmd += " -to " + res;
+
+                // Encryption
+                do {
+                    debugln("Network encryption\n"
+                            "  open: Open network (no password)\n"
+                            "  wpa2: WPA2 protected network");
+                    CLI_READ_RES();
+                } while (!(res == "open" || res == "wpa2"));
+                cmd += " -enc " + res;
+
+                // Channel
+                do {
+                    debugln("Send on channel\n"
+                            "  1-14: WiFi channel to send packets on");
+                    CLI_READ_RES();
+                } while (!(res.toInt() >= 1 && res.toInt() <= 14));
+                cmd += " -ch " + res;
+
+                // Time
+                do {
+                    debugln("Stop attack\n"
+                            "   0: Never\n"
+                            "  >0: After x seconds");
+                    CLI_READ_RES();
+                } while (!(res.toInt() >= 0));
+                cmd += " -t " + res;
+            } else if (res == "deauth") {}
 
             // Result
             for (int i = 0; i<cmd.length()+2; ++i) debug('#');
@@ -256,7 +293,7 @@ namespace cli {
         });
         cmd_beacon.addArg("s/sid/s");
         cmd_beacon.addArg("from,mac/from", "random");
-        cmd_beacon.addArg("to,macto", "FF:FF:FF:FF:FF:FF");
+        cmd_beacon.addArg("to,macto", "broadcast");
         cmd_beacon.addArg("enc/ryption", "open");
         cmd_beacon.addArg("ch/annel", "1");
         cmd_beacon.addArg("t/ime", "300");
@@ -362,22 +399,22 @@ namespace cli {
 
             attack::deauth(targets, deauth, disassoc, pkt_rate, timeout, max_pkts);
         });
-        cmd_deauth.addArg("m/ode", "deauth+disassoc");
         cmd_deauth.addArg("ap", "");
         cmd_deauth.addArg("st/ation", "");
         cmd_deauth.addArg("mac", "");
         cmd_deauth.addArg("t/ime/out", "300");
         cmd_deauth.addArg("n/um/ber", "0");
         cmd_deauth.addArg("r/ate", "20");
+        cmd_deauth.addArg("m/ode", "deauth+disassoc");
         cmd_deauth.setDescription(
             "  Deauthenticate (disconnect) selected WiFi connections\n"
-            "  -m:   packet types [deauth,disassoc,deauth+disassoc] (default=deauth+disassoc)\n"
             "  -ap:  access point IDs to attack\n"
             "  -st:  station IDs to attack\n"
             "  -mac: manual target selection [MacFrom-MacTo-Channel] for example:'aa:bb:cc:dd:ee:ff-00:11:22:33:44:55-7'\n"
             "  -t:   attack timeout in seconds (default=300)\n"
             "  -n:   packet limit [>1] (default=0)\n"
-            "  -r:   packets per second (default=20)"
+            "  -r:   packets per second (default=20)\n"
+            "  -m:   packet types [deauth,disassoc,deauth+disassoc] (default=deauth+disassoc)"
             );
 
         Command cmd_clear = cli.addCommand("clear", [](cmd* c) {
