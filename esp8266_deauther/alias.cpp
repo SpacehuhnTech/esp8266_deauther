3f1767c35e79d2bf009164a54e808a68a6802585
Spacehuhn <github@spacehuhn.com>
2020-03-08 11:06:22 +0100

    Print alias list

--- esp8266_deauther/alias.cpp
+++ esp8266_deauther/alias.cpp
@@ -9,6 +9,7 @@
 #include "mac.h"
 #include "debug.h"
 #include "eeprom.h"
+#include "vendor.h"
 
 typedef struct alias_t {
     uint8_t mac[6];
@@ -67,57 +68,60 @@ namespace alias {
     }
 
     bool add(const uint8_t* mac, const String& name) {
-        if ((list.size < MAX_ALIAS_NUM) && (search(mac) < 0) && (search(name) < 0)) {
-            alias_t* a = &list.data[list.size];
-            memcpy(a->mac, mac, 6);
-            strncpy(a->name, name.c_str(), MAX_ALIAS_LEN);
-            ++list.size;
-            save();
-            return true;
-        }
-        return false;
+        if ((list.size >= MAX_ALIAS_NUM) ||
+            (search(mac) > 0) ||
+            (search(name) > 0)) return false;
+
+        alias_t* a = &list.data[list.size];
+        memcpy(a->mac, mac, 6);
+        strncpy(a->name, name.c_str(), MAX_ALIAS_LEN);
+        ++list.size;
+        save();
+
+        return true;
     }
 
     String get(const uint8_t* mac) {
         int id = search(mac);
 
-        if ((id >= 0) && (id <= list.size)) {
-            String res;
+        if ((id < 0) || (id > list.size)) return strh::mac(mac);
 
-            for (unsigned int j = 0; j<MAX_ALIAS_LEN; ++j) {
-                res += char(list.data[id].name[j]);
-            }
-
-            return res;
-        }
-
-        return strh::mac(mac);
+        return getName(id);
     }
 
     bool resolve(const String& name, uint8_t* buffer) {
         int id = search(name);
 
-        if ((id >= 0) && (id <= list.size)) {
-            memcpy(buffer, list.data[id].mac, 6);
-            return true;
+        if ((id < 0) || (id > list.size)) return false;
+
+        memcpy(buffer, list.data[id].mac, 6);
+        return true;
+    }
+
+    String getName(int id) {
+        if ((id < 0) || (id > list.size)) return String();
+
+        String res;
+
+        for (unsigned int i = 0; i<MAX_ALIAS_LEN && list.data[id].name[i] != '\0'; ++i) {
+            res += char(list.data[id].name[i]);
         }
 
-        return false;
+        return res;
     }
 
     bool remove(int id) {
-        if ((id >= 0) && (id <= list.size)) {
-            for (unsigned int i = id; i<list.size && i<MAX_ALIAS_NUM; ++i) {
-                alias_t* c = &list.data[i];
-                alias_t* n = &list.data[i+1];
-                memcpy(c->mac, n->mac, 6);
-                strncpy(c->name, n->name, MAX_ALIAS_LEN);
-            }
-            --list.size;
-            save();
-            return true;
+        if ((id < 0) || (id > list.size)) return false;
+
+        for (unsigned int i = id; i<list.size && i<MAX_ALIAS_NUM; ++i) {
+            alias_t* c = &list.data[i];
+            alias_t* n = &list.data[i+1];
+            memcpy(c->mac, n->mac, 6);
+            strncpy(c->name, n->name, MAX_ALIAS_LEN);
         }
-        return false;
+        --list.size;
+        save();
+        return true;
     }
 
     bool remove(const uint8_t* mac) {
@@ -132,17 +136,27 @@ namespace alias {
         debug("MAC Alias List: ");
         debugln(list.size);
 
-        for (unsigned int i = 0; i<list.size && i<MAX_ALIAS_NUM; ++i) {
-            debug(i);
-            debug(" ");
-            debug(strh::mac(list.data[i].mac));
-            debug(" ");
+        debug(strh::right(3, "ID"));
+        debug(' ');
+        debug(strh::left(MAX_ALIAS_LEN, "Name"));
+        debug(' ');
+        debug(strh::left(17, "MAC-Address"));
+        debug(' ');
+        debug(strh::left(8, "Vendor"));
+        debugln();
+        debugln("===========================================");
 
-            for (unsigned int j = 0; j<MAX_ALIAS_LEN; ++j) {
-                debug(list.data[i].name[j]);
-            }
+        for (unsigned int i = 0; i<list.size && i<MAX_ALIAS_NUM; ++i) {
+            debug(strh::right(3, String(i)));
+            debug(' ');
+            debug(strh::left(MAX_ALIAS_LEN, getName(i)));
+            debug(' ');
+            debug(strh::left(17, strh::mac(list.data[i].mac)));
+            debug(' ');
+            debug(strh::left(8, vendor::search(list.data[i].mac)));
             debugln();
         }
+        debugln("===========================================");
 
         debugln();
     }
--- esp8266_deauther/alias.h
+++ esp8266_deauther/alias.h
@@ -20,6 +20,8 @@ namespace alias {
     String get(const uint8_t* mac);
     bool resolve(const String& name, uint8_t* buffer);
 
+    String getName(int id);
+
     bool remove(int id);
     bool remove(const uint8_t* mac);
     bool remove(const String& name);
