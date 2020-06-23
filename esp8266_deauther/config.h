fc2bd83b5ac7681bb10c4114fd90f01224fb4c9a
Spacehuhn <github@spacehuhn.de>
2020-06-23 20:46:22 +0200

    Updated version
    Fuck, forgot to do this the entire day

--- esp8266_deauther/config.h
+++ esp8266_deauther/config.h
@@ -6,7 +6,7 @@
 
 #pragma once
 
-#define VERSION "Version 3.0.0 dev 2020-05-30#4"
+#define VERSION "Version 3.0.0 dev 2020-06-23#1"
 
 // ===== Serial Communication ===== //
 #define ENABLE_DEBUG      // Enable serial logging
