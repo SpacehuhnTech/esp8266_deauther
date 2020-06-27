63557c81a972a1ec6cc9f5bba324c39cc1c24783
Spacehuhn <github@spacehuhn.de>
2020-06-27 15:54:33 +0200

    Fix AP settings print bug
    It was showing WPA2 even if no password was set

--- esp8266_deauther/ap.cpp
+++ esp8266_deauther/ap.cpp
@@ -141,7 +141,7 @@ namespace ap {
         debugln(ap_settings.pswd);
 
         debugF("Mode:      ");
-        debugln(ap_settings.pswd ? "WPA2" : "Open");
+        debugln(ap_settings.pswd == '\0' ? "WPA2" : "Open");
 
         debugF("Hidden:    ");
         debugln(strh::boolean(ap_settings.hidden));
